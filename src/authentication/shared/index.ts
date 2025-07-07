729d0a294cef7eb6273de6db8c51a62da11609eb
Ahmed Abbas <powerfactor@hotmail.com>
2025-07-07 08:44:20 +0400

    Refactored to split auth and state

--- src/authentication/shared/index.ts
+++ src/authentication/shared/index.ts
@@ -2,3 +2,4 @@ export * from './constants/cookieOptions';
 export * from './middleware';
 export * from './types';
 export * from './utils/mapDecodedToAuthUser';
+export { AuthProvider as AuthProviderValue } from './AuthProvider';
