83eb52f5f7349c4fc78860ab8608fff89907feb3
Ahmed Abbas <powerfactor@hotmail.com>
2025-07-08 18:40:10 +0400

    Refactored to split auth and state

--- src/authentication/shared/types/Auth.ts
+++ src/authentication/shared/types/Auth.ts
@@ -1,7 +1,7 @@
 // src/authentication/shared/types/Auth.ts
 
 import type { NextApiRequest } from 'next';
-import type { Auth } from 'firebase-admin/auth';
+import type { Auth } from 'firebase/auth';
 import { AuthUser } from '@authentication/shared';
 
 export interface AuthContextType {
