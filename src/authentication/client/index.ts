f185c13269bb0aa6013591bf4c5be2c19b62d62a
Ahmed Abbas <powerfactor@hotmail.com>
2025-07-10 00:26:19 +0400

    Refactored to split auth and state

--- src/authentication/client/index.ts
+++ src/authentication/client/index.ts
@@ -2,6 +2,7 @@ export * from './hooks/useAuth';
 export * from './hooks/useSignIn';
 export * from './hooks/useSignOut';
 export * from './hooks/useBackend';
+export * from './hooks/useBackendMutation';
 export * from './utils/withAuthProtection';
 export * from './utils/backendClient';
 export * from './components/AuthProtection';
